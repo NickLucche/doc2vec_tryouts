4fcc90a3cc8af28c6ffa8720ceecddcb0eeece06
NickLucche <nicolo.lucchesi@gmail.com>
2018-10-19 11:02:14 +0200

    Added modules to use in model evaluation notebook

---
+++ my_dbscan.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module containing utils used for clustering.
+@author: NickLucche
+"""
+
+from sklearn.cluster import DBSCAN
+
+# my function for performing dbscan and printing out cluster results
+def perform_dbscan(eps = 0.4, min_samples = 4, metric = 'euclidean', algorithm = 'auto', data = None, verbose = True
+                  , titles = None, urls = None, print_noise = True):
+    """perform DBSCAN over given data, using given parametrs. Returns dbscan object and clusters dictionary."""
+    
+    db = DBSCAN(eps=eps, min_samples=min_samples, metric=metric, algorithm=algorithm).fit(data)
+
+    # labels will print out the number of the cluster each example belongs to;
+    # -1 if the vector is considered noise (not belonging to any cluster)
+    #print("Labels: ", db.labels_)
+
+    # create data structure containing clusters
+    clusters_to_ret = {label:[] for label in db.labels_ if label!=-1}
+    
+    for i, label in enumerate(db.labels_):
+        if label != -1: #ignore noise points
+            clusters_to_ret[label].append(urls[i])
+        
+    
+    
+    # only do this if you need to print out the result (messy for large number of docs)
+    if verbose:
+        print("##Clusters##")
+        clusters = {label: [] for label in db.labels_ if label!=-1}
+        noise = []
+        for i, label in enumerate(db.labels_):
+            if label != -1: 
+                clusters[label].append(titles[i])
+            else: # save noise points
+                noise.append(titles[i])
+                
+        for label, list_ in clusters.items():
+            print("Cluster: {}".format(list_))
+        if print_noise:
+            print("Noise: ", noise)
+
+        print("DBSCAN finished.\n")
+    return db, clusters_to_ret
+
+def apply_dbscan(doc_vecs, titles, urls, subset_length, eps = 0.27, eps_increment = 0.1, n_iterations = 1, verbose = False):
+    """
+        This method performs a dbscan clustering and returns the resulting clusters,
+        as a list of urls.
+        Parameters are pretty self-explanatory; multiple iterations version doesn't yet implement
+        a heuristic.
+    """
+    # TODO: improve description
+    
+    # subset of docs vectors 
+    subset = doc_vecs[:subset_length]
+    subset_titles = titles[:subset_length]
+    sub_urls = urls[:subset_length]
+    
+    noise_bool = False
+    # this will contain all clusters found, each one as a list, 
+    # mantaining the order dbscan returned (first clusters will contain articles more related to each other)
+    final_clusters = []
+    # starting eps will be the sum of eps + eps_increment 
+    for i in range(n_iterations):
+        if i==2: 
+            noise_bool = True
+        eps = eps + eps_increment
+       
+        db, clusters = perform_dbscan(eps = eps, min_samples = 2, metric = 'cosine', algorithm = 'auto',
+                            data = subset, verbose = True, titles = subset_titles, urls = sub_urls, print_noise = noise_bool)
+    
+        # TODO: ignore noise/'other' documents or return them?
+        for label, list_ in clusters.items():
+            final_clusters.append(list_)
+            
+        # let's try and find other clusters in the noise data, with higher eps
+        subset = [subset[i] for i, label in enumerate(db.labels_) if label==-1]
+        subset_titles = [subset_titles[i] for i, label in enumerate(db.labels_) if label==-1]
+        sub_urls = [sub_urls[i] for i, label in enumerate(db.labels_) if label==-1]
+        if subset is None:
+            break
+    
+    if verbose:
+        print("Number of cluster found: ", len(final_clusters))
+        for i, cluster in enumerate(final_clusters):
+            print("Length of cluster {0}: {1}".format(i, len(cluster)))
+    # final clusters composition:
+    #[[cluster0_urls], [cluster1_urls], ...]
+    return final_clusters
---
+++ visualization_utils.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module containing methods used to visualize results in plots.
+
+@author: NickLucche
+"""
+#TODO: improve description
+import plotly.plotly as py
+import plotly.tools as tls
+import plotly.graph_objs as go
+import random
+import numpy as np
+import sklearn.metrics.pairwise as sk # for cosine_distance
+
+def plot_clusters(clusters, filename = '' ):
+    """
+        Clusters: list of clusters, each cluster containing a document representation (its title, url, id..)
+        
+        This method will plot the given clusters, in a non-meaningful way in terms of clusters
+        position in space, but rather focusing on clearly distinguishing clusters and 
+        on showing their content.
+    """
+    # using my api-key
+    tls.set_credentials_file(username='D4nt3', api_key='FdMB4O6qCfciGDOnLvdQ')
+    
+    
+    
+    traces = []
+    c_colors = ['blue', 'red', 'yellow', 'green', 'pink', 'grey', 'black', 'magenta']
+    
+    # each trace will represent a cluster; the more documents a cluster contains,
+    # the bigger the point visualized will be.
+    # Hovering on point will pop-up a list showing cluster content.
+    for i, cluster in enumerate(clusters):
+        # assign a color to each cluster
+        
+        x , y = random.randint((-1)**(i), (-3)**(i)), random.randint((-1)**(i), (-3)**(i))
+        color = c_colors[i]
+        text = ''
+        for word in cluster:
+            text = text+','+word
+            
+        trace0 = go.Scatter(
+            x = [x], 
+            y = [y],
+            mode = 'markers',
+            name = 'cluster'+str(i),
+            marker = dict(
+                size = 7,
+                color = color,
+            ),
+            text = text
+        )
+        traces.append(trace0)
+    
+    
+    data = traces 
+    layout = dict(title = 'Clusters visual representation',
+                hovermode= 'closest',
+                xaxis= dict(
+                    title= 'x',
+                    ticklen= 5,
+                    gridwidth= 2,
+                ),
+                yaxis=dict(
+                    title= 'y',
+                    ticklen= 5,
+                    gridwidth= 2,
+                ),
+                showlegend = False
+            )
+    # Plot and embed in ipython notebook!
+        
+    fig = dict(data = data, layout = layout)
+    py.iplot(fig, filename= filename)
+    # or return fig
+    
+def choose_eps(min_count, docs_vecs, doc_titles):
+    """Plot the graph and choose best eps based on the composition of our data: 
+    to do so, we will need to compute the distances between every point in the data-space, and its
+    2nd/3rd closest neighbour (based on 'min_count'). 
+    Take the eps corresponding to a great change in the derivative of the plotted function ('knee' or 'elbow' shape).
+    
+    Docs_vecs is the list of vectors we will analyze, each representing a document.
+    
+    min_count is the number of points needed to define a core point in DBSCAN.
+    
+    doc_titles is a matching list (wrt to docs_vecs), containing the titles of each doc.
+    
+    Returns a list of tuples (doc_title, distance from k-th neighbour), ORDERED by distance (ascendantly).
+    """
+    
+    # first thing to do: compute the matrix of all pairwise elements distances
+    # warning: this code is not optimized
+    dist_matrix = get_pairwise_distances_matrix(docs_vecs)
+    
+    
+    list_ = [] 
+    # for each document vec, only keep the DISTANCE from k-th closest document
+    for j, doc_distances in enumerate(dist_matrix):
+        # get a row of the matrix (vector of distances for doc_j)
+        
+        # discard the distance between a doc and itself
+        doc_distances = np.delete(doc_distances, j)
+        for i in range(0, min_count-1):
+            # get the closest doc to it and discard it, we only need the k-th closest doc.
+            doc_distances = np.delete(doc_distances, np.argmin(doc_distances))
+        # now create the pair: (doc_name, distance from k-th neighbour)
+        list_.append((doc_titles[j], np.amin(doc_distances)))
+        
+    # sort the list by the second parameter (distance)
+    list_.sort(key=lambda tup: tup[1])  # sorts in place
+    return list_
+
+
+# TODO: add possibility of passing metric to use as parameters
+def get_pairwise_distances_matrix(docs):
+    """"
+        docs: list of documents, each represented as a vector.
+        
+        Returns the pairwise distances matrix between documents. 
+    
+        Metric used to compute the distance is cosine_distance -by default-.
+    """
+    # initialize distance matrix
+    n = len(docs)
+    distances_m = np.zeros((n, n))
+    
+    # compute the distance betweem each vector (doc)
+    # this is all but efficient at the moment, okay for a debug version.
+    for i, doc1 in enumerate(docs):
+        for j, doc2 in enumerate(docs):
+            distances_m[i, j] = sk.cosine_distances([doc1], [doc2])
+    return distances_m
+
+def mean_of_vectors(vectors):
+    """given a list of vectors, return the simplest mean of vectors."""
+    
+    sum_vectors = np.zeros(np.shape(vectors[0]))
+    for vec in vectors:
+        sum_vectors = sum_vectors + vec
+    return sum_vectors/len(vectors)
+
+def infer_vector(entities, model):
+    """Given a list of entities, returns the vector representing the documents from which the entities 
+    were extracted from, wrt a given W2V model.
+    
+    entities: list of entities, our way of representing a document.
+    model: w2v model.
+    """
+    
+    # get word vector of each entity; ignores word if the model does not know it
+    entities_vecs = []
+    for e in entities:
+        try:
+            entities_vecs.append(model[e])
+        except:
+            None # ignore unknown word
+    
+    return mean_of_vectors(entities_vecs)
+    
+
+def visualize_eps_graph(data ,filename):
+    trace = go.Scatter(
+        x =[x for (x, y) in title_dist_tuples],  # list of x
+        y = [y for (x, y) in title_dist_tuples],
+        mode = 'lines',
+        name = 'lines'
+    )
+    
+    data = [trace]
+    
+    py.iplot(data, filename = filename)
